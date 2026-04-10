d4720f7f9ee57c70f75f52002f6a9a4db3a52a82
Mark Gent <Mark.Gent@homesengland.gov.uk>
2026-04-10 19:14:48 +0100

    localstack changes

--- infra/localstack/init/start-localstack.sh
+++ infra/localstack/init/start-localstack.sh
@@ -5,7 +5,6 @@ export AWS_REGION=eu-west-2
 export AWS_DEFAULT_REGION=eu-west-2
 export AWS_ACCESS_KEY_ID=test
 export AWS_SECRET_ACCESS_KEY=test
-export ENDPOINT_URL=http://localhost:4566
 
 set -e
 
@@ -20,9 +19,10 @@ existing_bucket=$(awslocal s3api list-buckets \
 if [[ "$existing_bucket" == "cads-internal-bucket" ]]; then
   echo "S3 bucket already exists: cads-internal-bucket"
 else
-  awslocal s3api create-bucket --bucket cads-internal-bucket --region eu-west-2 \
-    --create-bucket-configuration LocationConstraint=eu-west-2 \
-    --endpoint-url=http://localhost:4566
+  awslocal s3api create-bucket \
+    --bucket cads-internal-bucket \
+    --region eu-west-2 \
+    --create-bucket-configuration LocationConstraint=eu-west-2
   echo "S3 bucket created: cads-internal-bucket"
 fi
 
@@ -32,7 +32,6 @@ echo "Creating SQS resources..."
 ## S3: Create 'cads-cds-queue' DLQ
 dlq_url=$(awslocal sqs create-queue \
   --queue-name cads-cds-queue-deadletter \
-  --endpoint-url=http://localhost:4566 \
   --output text \
   --query 'QueueUrl')
 echo "'cads-cds-queue' DLQ created: $dlq_url"
@@ -41,7 +40,6 @@ echo "'cads-cds-queue' DLQ created: $dlq_url"
 dlq_arn=$(awslocal sqs get-queue-attributes \
   --queue-url "$dlq_url" \
   --attribute-names QueueArn \
-  --endpoint-url=http://localhost:4566 \
   --output text \
   --query 'Attributes.QueueArn')
 echo "'cads-cds-queue' DLQ ARN: $dlq_arn"
@@ -49,7 +47,6 @@ echo "'cads-cds-queue' DLQ ARN: $dlq_arn"
 # Create 'cads-cds-queue' queue
 queue_url=$(awslocal sqs create-queue \
   --queue-name cads-cds-queue \
-  --endpoint-url=http://localhost:4566 \
   --output text \
   --query 'QueueUrl')
 echo "'cads-cds-queue' queue created: $queue_url"
@@ -67,17 +64,7 @@ EOF
 echo "Set redrive policy for 'cads-cds-queue' DLQ..."
 awslocal sqs set-queue-attributes \
   --queue-url "$queue_url" \
-  --attributes "{\"RedrivePolicy\":\"{\\\"deadLetterTargetArn\\\":\\\"$dlq_arn\\\",\\\"maxReceiveCount\\\":\\\"3\\\"}\"}" \
-  --endpoint-url=$ENDPOINT_URL
+  --attributes "{\"RedrivePolicy\":\"{\\\"deadLetterTargetArn\\\":\\\"$dlq_arn\\\",\\\"maxReceiveCount\\\":\\\"3\\\"}\"}"
 # =================================================================
-
-# Get the 'cads-cds-queue' queue ARN
-queue_arn=$(awslocal sqs get-queue-attributes \
-  --queue-url "$queue_url" \
-  --attribute-name QueueArn \
-  --endpoint-url=http://localhost:4566 \
-  --output text \
-  --query 'Attributes.QueueArn')
-echo "'cads-cds-queue' queue ARN: $queue_arn"
 
 echo "LocalStack Bootstrapping Complete"
